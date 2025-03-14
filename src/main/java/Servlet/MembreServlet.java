35bfb80c31ce924227552afa5d85153d4af46a62
youssef eddib <eddiby79@gmail.com>
2025-03-14 15:01:25 +0100

    le fichier jsp de la page Home

--- src/main/java/Servlet/MembreServlet.java
+++ src/main/java/Servlet/MembreServlet.java
@@ -5,11 +5,11 @@ import Model.MembreModel;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
-import javax.Servlet.ServletException;
-import javax.Servlet.annotation.WebServlet;
-import javax.Servlet.http.HttpServlet;
-import javax.Servlet.http.HttpServletRequest;
-import javax.Servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/MembreServlet")
 public class MembreServlet extends HttpServlet {
@@ -22,20 +22,29 @@ public class MembreServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<MembreModel> membres = membreDAO.getAllMembres();
+        List<MembreModel> membres = null;
+        try {
+            membres = membreDAO.getAllMembres();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         request.setAttribute("membres", membres);
         request.getRequestDispatcher("membres.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException {
+            throws ServletException, IOException {
         String nom = request.getParameter("nom");
         String prenom = request.getParameter("prenom");
         String email = request.getParameter("email");
         String telephone = request.getParameter("telephone");
 
         MembreModel membre = new MembreModel(0, nom, prenom, email, telephone);
-        membreDAO.ajouterMembre(membre);
+        try {
+            membreDAO.ajouterMembre(membre);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         response.sendRedirect("MembreServlet");
     }
