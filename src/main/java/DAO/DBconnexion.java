5163da73adc2e7e794edba52a6b22a29a9b1ebeb
youssef eddib <eddiby79@gmail.com>
2025-03-14 10:25:03 +0100

    DAO de la page Membre

--- src/main/java/DAO/DBconnexion.java
+++ src/main/java/DAO/DBconnexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBconnexion {
     private static final String URL = "jdbc:mysql://localhost:3306/SportFlow";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "youssef";
 
     public static Connection getConnection() {
         Connection connection = null;
